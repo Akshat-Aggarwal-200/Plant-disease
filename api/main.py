fb7cc8388b64d125b5e044f752cab8ac3edeb6e4
Akshat-Aggarwal-200 <akshataggarwal6004@gmail.com>
2023-12-01 11:07:28 +0530

    Starting Frontend with react completed backend

--- api/main.py
+++ api/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
 
 
 MODEL = tf.keras.models.load_model("model\classifiers")
-class_names = os.listdir("dataset")
+class_names = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Cherry_(including_sour)___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Grape___Black_rot','Grape___Esca_(Black_Measles)', 'Grape___healthy', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Peach___Bacterial_spot', 'Peach___healthy']
 
 @app.get("/")
 def index():
@@ -39,10 +39,18 @@ def index():
 async def predict(file : UploadFile = File(...)):
     #Read File
     image = read_image(await file.read())
-    img_batch = np.expand_dims(image,0)
-    predict = MODEL.predict(img_batch)
-    PREDICTED_CLASS = class_names[np.argmax(predict[0])]
-    confidence = np.argmax(predict[0])
+    image = tf.image.resize(image,(224,224))
+    img_array = tf.keras.preprocessing.image.img_to_array(image)
+    img_array = tf.expand_dims(img_array, 0)
+    # # img_array
+    predict = MODEL.predict(img_array)
+    # return {
+    #     'predict': predict[0].tolist()
+    #         }
+    pre = predict[0].tolist()
+    confidence = max(pre)
+    PREDICTED_CLASS = class_names[pre.index(confidence)]
+    # # pass
     return {
         'class' : PREDICTED_CLASS,
         'confidence' : confidence
